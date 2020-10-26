ce7f7764e7c93a4f6fb57c9810d18995db2b9623
MiLinSoQ <bluejacky258@gmail.com>
2020-10-26 18:11:12 +0800

    Binary tree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,3 +1,7 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class BinaryTree<E extends Comparable<E>> {
 	
 	private class Node {
@@ -30,7 +34,7 @@ public class BinaryTree<E extends Comparable<E>> {
 		return this.size;
 	}
 	
-	public boolean isEmpy() {
+	public boolean isEmpty() {
 		return this.size == 0;
 	}
 	
@@ -130,6 +134,164 @@ public class BinaryTree<E extends Comparable<E>> {
 		postOrder(node.right);
 		System.out.print(node.val + " -> ");
 	}
+	
+	// 層序走訪
+	public void levelOrder() {
+		if (this.root == null) return;
+		
+		Queue<Node> queue = new  LinkedList<>();
+		queue.add(this.root);
+		
+		while (!queue.isEmpty()) {
+			Node cur = queue.remove();
+			System.out.print(cur.val + " -> ");
+			if (cur.left != null) queue.add(cur.left);
+			if (cur.right != null) queue.add(cur.right);
+		}
+	}
+	
+	// 找出最小值
+	public E minimum() {
+		if (isEmpty()) throw new IllegalArgumentException("Minimum failed, Binary tree is empty.");
+		return minimum(this.root).val;
+	}
+	
+	private Node minimum(Node node) {
+		if (node.left == null) return node;
+		return minimum(node.left);
+	}
+	
+	// 找出最大值
+	public E maximum() {
+		if (isEmpty()) throw new IllegalArgumentException("Minimum failed, Binary tree is empty.");
+		return maximum(this.root).val;
+	}
+	
+	private Node maximum(Node node) {
+		if (node.right == null) return node;
+		return maximum(node.right);
+	}
+	
+	// 刪除最小值
+	public E removeMin() {
+		E min = minimum();
+		this.root = removeMin(this.root);
+		return min;
+	}
+	
+	private Node removeMin(Node node) {
+		if (node.left == null) {
+			Node rightNode = node.right;
+			node.right = null;
+			size--;
+			return rightNode;
+		}
+		node.left = removeMin(node.left);
+		return node;
+	}
+	
+	// 刪除最大值
+	public E removeMax() {
+		E max = maximum();
+		this.root = removeMax(this.root);
+		return max;
+	}
+	
+	private Node removeMax(Node node) {
+		if (node.right == null) {
+			Node leftNode = node.left;
+			node.left = null;
+			size--;
+			return leftNode;
+		}
+		node.right = removeMax(node.right);
+		return node;
+	}
+	
+	// 刪除值
+	public void remove(E val) {
+		this.root = remove(this.root, val);
+	}
+	
+	private Node remove(Node node, E val) {
+		if (node == null) return node;
+		
+		if (val.compareTo(node.val) < 0) {
+			node.left = remove(node.left, val);
+			return node;
+		} else if (val.compareTo(node.val) > 0) {
+			node.right = remove(node.right, val);
+			return node;
+		}  else { // val == node.val
+			
+			if (node.left == null) {
+				
+				Node rightNode = node.right;
+				node.right = null;
+				size--;
+				return rightNode;
+			}
+			
+			if (node.right == null) {
+				
+				Node leftNode = node.left;
+				node.left = null;
+				size--;
+				return leftNode;
+			}
+			
+			Node successor = minimum(node.right);
+			successor.right = removeMin(node.right);
+			successor.left = node.left;
+			
+			node.left = node.right = null;
+			return successor;
+		}
+	}
+	
+	// 找到比 val 小的最大值
+	public E floor(E val) {
+		if (isEmpty()) throw new IllegalArgumentException("Get floor failed, Binary tree is empty.");
+		Node floorNode = floor(this.root, val);
+		if (floorNode == null) return val;
+		return floorNode.val;
+	}
+	
+	private Node floor(Node node, E val) {
+		if (node == null || val == node.val) return node;
+		
+		if (val.compareTo(node.val) < 0) {
+			return floor(node.left, val);
+		}
+		
+		Node floorNode = floor(node.right, val);
+		if (floorNode == null) return node;
+		return floorNode.val.compareTo(val) <= 0 ? floorNode : node;
+	}
+	
+	
+	// 找到比 val 大的最小值
+	public E ceil(E val) {
+		if (isEmpty()) throw new IllegalArgumentException("Get ceil failed, Binary tree is empty.");
+		Node ceilNode = ceil(this.root, val);
+		if (ceilNode == null) return val;
+		return ceilNode.val;
+	}
+	
+	private Node ceil(Node node,E val) {
+		if (node == null || val  == node.val) return node;
+		
+		
+		if (val.compareTo(node.val) > 0) {
+			return ceil(node.right, val);
+		}
+		
+		Node ceilNode = ceil(node.left, val);
+		if (ceilNode == null) return node;
+		
+		return ceilNode.val.compareTo(val) >= 0 ? ceilNode : node;
+	}
+	
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -11,31 +11,62 @@ public class Main {
 		System.out.println("------------");
 		
 		BinaryTree tree = new BinaryTree();
-		int[] nums = { 5, 3, 6, 8, 4, 2 };
+		int[] nums = { 8, 4, 12, 2, 6, 10, 14};
 
 		for (int i = 0; i < nums.length; i++) {
 			tree.add(nums[i]);
 		}
 
 		/**
-				5
-			  /   \
-			 3	   6 
-		   /   \	 \
-		  2	    4	  8
+				          8
+						/   \    
+					  4      12
+					/  \    /  \
+				   2    6  10   14
 
 		*/
 
 		System.out.println("Pre order.");
 		tree.preOrder();
-
+		System.out.println("");
+		
 		System.out.println("In order.");
 		tree.inOrder();
-
+		System.out.println("");
+		
 		System.out.println("Post order.");
 		tree.postOrder();
-
+		System.out.println("");
+		
+		System.out.println("Level Order.");
+		tree.levelOrder();
+		
+		System.out.println("");
 		System.out.println("------------");
+		
+		
+		System.out.println(String.format("Minimum: [ %d ]", tree.minimum()));
+		System.out.println(String.format("Maximum: [ %d ]", tree.maximum()));
+		
+		
+		System.out.println(String.format("Floor: [ %d ]", tree.floor(11)));
+		
+		System.out.println(String.format("Ceil: [ %d ]", tree.ceil(11)));
+		
+		// System.out.println(String.format("Remove mim: [ %d ]", tree.removeMin()));
+		// System.out.println("Pre order.");
+		// tree.preOrder();
+		// System.out.println("");
+		
+		// System.out.println(String.format("Remove max: [ %d ]", tree.removeMax()));
+		// System.out.println("Pre order.");
+		// tree.preOrder();
+		// System.out.println("");
+		
+		// tree.remove(4);
+		// System.out.println("Pre order.");
+		// tree.preOrder();
+		// System.out.println("");
 	}
 	
 	private static void LinkedListTest() {
