ef57c14c19b139572b889ff91e93f4c165218b0f
MiLinSoQ <bluejacky258@gmail.com>
2020-10-28 18:14:14 +0800

    add BinaryTree

---
+++ src/BinarySearchTree/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("------------");
+		
+		BinaryTree tree = new BinaryTree();
+		int[] nums = { 8, 4, 12, 2, 6, 10, 14};
+
+		for (int i = 0; i < nums.length; i++) {
+			tree.add(nums[i]);
+		}
+
+		/**
+				          8
+						/   \    
+					  4      12
+					/  \    /  \
+				   2    6  10   14
+		*/
+
+		System.out.println("Pre order.");
+		tree.preOrder();
+		System.out.println("");
+		
+		System.out.println("In order.");
+		tree.inOrder();
+		System.out.println("");
+		
+		System.out.println("Post order.");
+		tree.postOrder();
+		System.out.println("");
+		
+		System.out.println("Level Order.");
+		tree.levelOrder();
+		
+		System.out.println("");
+		System.out.println("------------");
+		
+		
+		System.out.println(String.format("Minimum: [ %d ]", tree.minimum()));
+		System.out.println(String.format("Maximum: [ %d ]", tree.maximum()));
+		
+		
+		System.out.println(String.format("Floor: [ %d ]", tree.floor(11)));
+		
+		System.out.println(String.format("Ceil: [ %d ]", tree.ceil(11)));
+		
+		// System.out.println(String.format("Remove mim: [ %d ]", tree.removeMin()));
+		// System.out.println("Pre order.");
+		// tree.preOrder();
+		// System.out.println("");
+		
+		// System.out.println(String.format("Remove max: [ %d ]", tree.removeMax()));
+		// System.out.println("Pre order.");
+		// tree.preOrder();
+		// System.out.println("");
+		
+		// tree.remove(4);
+		// System.out.println("Pre order.");
+		// tree.preOrder();
+		// System.out.println("");
+		
+		System.out.println(tree.toString());
+		
+	}
+}
