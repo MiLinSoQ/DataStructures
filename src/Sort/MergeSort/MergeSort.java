f525183abb007bfb1170898a98f58d80c2a1f4b0
MiLinSoQ <bluejacky258@gmail.com>
2021-01-21 13:50:17 +0800

    Update MergeSort.

--- src/Sort/MergeSort/MergeSort.java
+++ src/Sort/MergeSort/MergeSort.java
@@ -5,36 +5,42 @@ public class MergeSort {
 	private MergeSort() { };
 	
 	public static <E extends Comparable<E>> void sort(E[] data) {
-		sort(data, 0, data.length - 1);
+		E[] temp = Arrays.copyOf(data, data.length);
+		sort(data, 0, data.length - 1, temp);
 	}
 	
-	private static <E extends Comparable<E>> void sort(E[] data, int l, int r) {
+	private static <E extends Comparable<E>> void sort(E[] data, int l, int r, E[] temp) {
 		if (l >= r) return;
 		
 		int mid = l + (r - l) / 2;
 		
-		sort(data, l, mid);
-		sort(data, mid + 1, r);
+		sort(data, l, mid, temp);
+		sort(data, mid + 1, r, temp);
 		
-		merge(data, l, mid, r);
+		if (data[mid].compareTo(data[mid + 1]) > 0) {
+			merge(data, l, mid, r, temp);
+		}
 	}
 	
 	// 
-	private static <E extends Comparable<E>> void merge(E[] data, int l, int mid, int r) {
-		E[] temp = Arrays.copyOfRange(data, l, r + 1);
+	private static <E extends Comparable<E>> void merge(E[] data, int l, int mid, int r, E[] temp) {
+		// E[] temp = Arrays.copyOfRange(data, l, r + 1);
+		
+		System.arraycopy(data, l, temp, l, r - l + 1);
+		
 		int i = l, j = mid + 1;
 		for (int k = l; k <= r; k ++) {
 			if (i > mid) {
-				data[k] = temp[j - l];
+				data[k] = temp[j];
 				j ++;
 			} else if (j > r) {
-				data[k] = temp[i - l];
+				data[k] = temp[i];
 				i ++;
-			} else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
-				data[k] = temp[i - l];
+			} else if (temp[i].compareTo(temp[j]) <= 0) {
+				data[k] = temp[i];
 				i ++;
 			} else {
-				data[k] = temp[j - l];
+				data[k] = temp[j];
 				j ++;
 			}
 		}
